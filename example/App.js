ffb09fc32e3136d805cd072da5371a5676be0d35
sevval <sevvalleygull@gmail.com>
2020-09-08 21:51:27 +0300

    next image added

--- example/App.js
+++ example/App.js
@@ -19,7 +19,7 @@ const App = () => {
           disableDescriptionText={true}
           count={3}
           LinearGradientColors={['#F4A09C', '#EE8F66', '#E83A30']}
-          imageSource={require('./lib/assets/bread.png')}
+          imageSource={require('./lib/asset/bread.png')}
           onPress={() => console.log('Bakery')}
         />
       </View>
@@ -29,7 +29,7 @@ const App = () => {
           descriptionText={'Butcher'}
           count={1}
           LinearGradientColors={['#FAF9D1', '#EECA66', '#E8B730']}
-          imageSource={require('./lib/assets/meat.png')}
+          imageSource={require('./lib/asset/meat.png')}
           onPress={() => console.log('Meat & Poultry')}
         />
       </View>
@@ -39,7 +39,7 @@ const App = () => {
           count={4}
           disableDescriptionText={true}
           LinearGradientColors={['#D1F3FA', '#9CE5F4', '#16A9C7']}
-          imageSource={require('./lib/assets/dairy.png')}
+          imageSource={require('./lib/asset/dairy.png')}
           onPress={() => console.log('Dairy & Eggs')}
         />
       </View>
@@ -49,7 +49,7 @@ const App = () => {
           count={8}
           disableDescriptionText={true}
           LinearGradientColors={['#E5D1FA', '#C69CF4', '#A866EE']}
-          imageSource={require('./lib/assets/supermarket.png')}
+          imageSource={require('./lib/asset/supermarket.png')}
           onPress={() => console.log('Pantry')}
         />
       </View>
@@ -58,7 +58,7 @@ const App = () => {
         count={2}
         disableDescriptionText={true}
         LinearGradientColors={['#D1FAD7', '#9CF4A7', '#66EE78']}
-        imageSource={require('./lib/assets/clean.png')}
+        imageSource={require('./lib/asset/clean.png')}
         onPress={() => console.log('Detergent')}
       />
     </View>
