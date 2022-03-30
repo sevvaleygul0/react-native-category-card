75a97858beda8800b78f7447e162d50b8be92d46
sevvaleygul0 <sevvalleygull@gmail.com>
2022-03-30 12:12:31 +0300

    feat: new image is added for card image

--- example/App.js
+++ example/App.js
@@ -19,7 +19,7 @@ const App = () => {
           disableDescriptionText={true}
           count={3}
           gradientColors={['#F4A09C', '#EE8F66', '#E83A30']}
-          imageSource={require('./lib/asset/bananas.png')}
+          imageSource={require("./lib/asset/star.png")}
           onPress={() => console.log('Bakery')}
         />
       </View>
@@ -29,7 +29,7 @@ const App = () => {
           descriptionText={'Butcher'}
           count={1}
           gradientColors={['#FAF9D1', '#EECA66', '#E8B730']}
-          imageSource={require('./lib/asset/bananas.png')}
+          imageSource={require("./lib/asset/star.png")}
           onPress={() => console.log('Meat & Poultry')}
         />
       </View>
@@ -39,7 +39,7 @@ const App = () => {
           count={4}
           disableDescriptionText={true}
           gradientColors={['#D1F3FA', '#9CE5F4', '#16A9C7']}
-          imageSource={require('./lib/asset/bananas.png')}
+          imageSource={require("./lib/asset/star.png")}
           onPress={() => console.log('Dairy & Eggs')}
         />
       </View>
@@ -49,7 +49,7 @@ const App = () => {
           count={8}
           disableDescriptionText={true}
           gradientColors={['#E5D1FA', '#C69CF4', '#A866EE']}
-          imageSource={require('./lib/asset/bananas.png')}
+          imageSource={require("./lib/asset/star.png")}
           onPress={() => console.log('Pantry')}
         />
       </View>
@@ -58,7 +58,7 @@ const App = () => {
         count={2}
         disableDescriptionText={true}
         gradientColors={['#D1FAD7', '#9CF4A7', '#66EE78']}
-        imageSource={require('./lib/asset/bananas.png')}
+        imageSource={require("./lib/asset/star.png")}
         onPress={() => console.log('Detergent')}
       />
     </View>
---
+++ example/lib/category-card/CategoryCard.js
@@ -0,0 +1,123 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {View, Image} from 'react-native';
+import Androw from 'react-native-androw';
+import {ScreenWidth} from '@freakycoder/react-native-helpers';
+import LinearGradient from 'react-native-linear-gradient';
+import Text from '@freakycoder/react-native-custom-text';
+import RNBounceable from '@freakycoder/react-native-bounceable';
+
+import styles, {_shadowStyle, _container} from './CategoryCard.style';
+
+export default class CategoryCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const {
+      count,
+      width,
+      height,
+      borderRadius,
+      titleText,
+      imageStyle,
+      shadowColor,
+      shadowStyle,
+      imageSource,
+      gradientColors,
+      descriptionText,
+      backgroundColor,
+      buttonImageStyle,
+      customImageComponent,
+      disableCountContainer,
+      disableDescriptionText,
+      ImageComponent = Image,
+      rightButtonImageSource,
+      customCountTextComponent,
+      customTitleTextComponent,
+      customDescriptionComponent,
+      onPress,
+    } = this.props;
+    return (
+      <RNBounceable onPress={onPress}>
+        <Androw style={shadowStyle || _shadowStyle(shadowColor)}>
+          <View
+            style={_container(height, width, borderRadius, backgroundColor)}>
+            <LinearGradient
+              start={{x: 0.0, y: 0.25}}
+              end={{x: 0.5, y: 1.0}}
+              colors={gradientColors}
+              style={styles.gradient}>
+              {customImageComponent || (
+                <ImageComponent
+                  style={imageStyle || styles.imageStyle}
+                  source={imageSource}
+                />
+              )}
+            </LinearGradient>
+            <View style={styles.textContainer}>
+              {customTitleTextComponent || (
+                <Text h4 bold>
+                  {titleText}
+                </Text>
+              )}
+              {!disableDescriptionText
+                ? customDescriptionComponent || (
+                    <Text h5 bold color={'#B4B4B4'}>
+                      {descriptionText}
+                    </Text>
+                  )
+                : null}
+            </View>
+            {!disableCountContainer
+              ? customCountTextComponent || (
+                  <View style={styles.countContainerStyle}>
+                    <Text h4 color={'#B4B4B4'}>
+                      {count}
+                    </Text>
+                  </View>
+                )
+              : null}
+            <View style={styles.rightButtonContainer}>
+              <Image
+                source={rightButtonImageSource}
+                style={buttonImageStyle || styles.buttonImageStyle}
+              />
+            </View>
+          </View>
+        </Androw>
+      </RNBounceable>
+    );
+  }
+}
+
+CategoryCard.propTypes = {
+  width: PropTypes.number,
+  count: PropTypes.number,
+  borderRadius: PropTypes.number,
+  height: PropTypes.number,
+  titleText: PropTypes.string,
+  shadowColor: PropTypes.string,
+  gradientColors: PropTypes.array,
+  descriptionText: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  disableCountContainer: PropTypes.bool,
+  disableDescriptionText: PropTypes.bool,
+};
+
+CategoryCard.defaultProps = {
+  count: 10,
+  borderRadius: 32,
+  height: 80,
+  shadowColor: '#757575',
+  backgroundColor: '#fff',
+  width: ScreenWidth * 0.8,
+  disableCountContainer: false,
+  descriptionText: 'Fresh Food',
+  disableDescriptionText: false,
+  titleText: 'Fruit & Vegetables',
+  gradientColors: ['#9CF4DF', '#30C9E8', '#107C91'],
+  rightButtonImageSource: require('../asset/next.png'),
+};
